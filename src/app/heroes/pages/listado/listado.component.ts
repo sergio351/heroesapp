7733151ca26377e8c2482270db4b94da0e464b71
sergio351 <velazquezsergio714@mail.com>
2022-01-06 19:12:51 -0300

    agregue cards

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interface/heroes.interface';
 
 @Component({
   selector: 'app-listado',
@@ -8,12 +9,13 @@ import { HeroesService } from '../../services/heroes.service';
   ]
 })
 export class ListadoComponent implements OnInit {
+  heroes: Heroe[]=[];
 
   constructor(private heroesservice : HeroesService) { }
 
   ngOnInit(): void {
       this.heroesservice.getHeroes()
-      .subscribe(resp=>console.log(resp));
+      .subscribe(heroes=>this.heroes=heroes);
   }
 
 }
